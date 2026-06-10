b1eec700e229f69e843bc3192d091341d1a088e2
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-06-10 17:46:42 +0530

    programs added for multiple operations

---
+++ Basic Introduction to Java/multipleoperators.java
@@ -0,0 +1,17 @@
+class multipleoperators{
+    public static void main(String[] args) {
+        float a=20;
+        float b=25;
+        float c=a+b;
+        float   c2=a-b;
+        float c3=a*b;
+        float c4=a/b;
+
+        System.out.println(".(addition is : )"+c);
+                System.out.println(".(substraction  is : )"+c2);
+            System.out.println(".(multi is : )"+c3);
+        System.out.println(".(division  is : )"+c4);
+
+        
+    }
+}
