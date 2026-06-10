4946d7ef5522f5f55a436929014501bfda2cb8ee
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-06-10 12:14:37 +0530

    added addition of numbers program

---
+++ Basic Introduction to Java/SimpleAdd.java
@@ -0,0 +1,9 @@
+public class SimpleAdd
+{
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c=a+b;
+        System.out.println(".(addition of the numbers is :)"+c);
+    }
+}
