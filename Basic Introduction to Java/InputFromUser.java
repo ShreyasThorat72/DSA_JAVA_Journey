dc9d2176353cf968bf0130962586690ab085ea28
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-06-10 18:06:59 +0530

    Input from users Program added

---
+++ Basic Introduction to Java/InputFromUser.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class InputFromUser
+{
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("number inputed is :"+input.nextInt());
+    }
+    
+}
