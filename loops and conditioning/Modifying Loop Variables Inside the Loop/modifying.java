11a8b17a100925fd8671041b4c2d34c5a32d39c9
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-06-21 12:28:34 +0530

    added modifying loops

---
+++ loops and conditioning/Modifying Loop Variables Inside the Loop/modifying.java
@@ -0,0 +1,15 @@
+
+class modifying{
+
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++)
+        {
+            if(i==2)
+            {
+                i++;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
