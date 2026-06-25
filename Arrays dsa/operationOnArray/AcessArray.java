2b4e0fd9455fb74d54d1c9ed73794c329f3dbb21
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-06-25 22:11:04 +0530

    added some folders

---
+++ Arrays dsa/operationOnArray/AcessArray.java
@@ -0,0 +1,10 @@
+class AcessArray
+{
+    public static void main(String[] args) {
+        
+        int arr[]={1,2,3,4,5,6,6,7};
+
+        System.out.println(arr[3]);
+          System.out.println(arr[0]);
+    }
+}
