cd89ce5842f3f9e277ce84c96944d59dd3d50677
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-06-25 19:36:26 +0530

    added non primitive folder

---
+++ Arrays dsa/Non Primitive array/nonPrimitive.java
@@ -0,0 +1,11 @@
+class nonPrimitive
+{
+    public static void main(String[] args) {
+        String arr[]={"shreyas","air","icy"};
+
+        for(int i=0;i<arr.length ; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
