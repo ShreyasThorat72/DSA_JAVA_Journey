0ba398884733bb2317795131a0e70b19980405aa
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-06-27 11:38:29 +0530

    added files

---
+++ Arrays dsa/operationOnArray/UpdateArray.java
@@ -0,0 +1,14 @@
+class UpdateArray
+{
+ public static void main(String[] args) {
+     
+     int arr[]={1,2,3,55,6,7,80};
+
+     arr[0]=90;
+     
+     for(int i=0;i<arr.length ;i++)
+     {
+        System.out.print(" "+arr[i]);
+     }
+ }
+}
