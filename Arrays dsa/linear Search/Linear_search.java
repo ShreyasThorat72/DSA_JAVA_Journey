f3ebd7b6231e65825a57389eb7dc3e40a05bbc10
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-07-07 20:57:13 +0530

    added linear search

---
+++ Arrays dsa/linear Search/Linear_search.java
@@ -0,0 +1,14 @@
+class Linear_search
+{
+    public static void main(String[] args) {
+        int arr[]={1,3,4,5,6,2};
+        
+        for(int i=0;i<arr.length;i++)
+        {
+        if(arr[i]==5)
+        {
+            System.out.println("the key value is found at index : "+i);
+        }
+        }
+    }
+}
--- Arrays dsa/rotating arrays main/Rotate_and_removeLast.java
+++ Arrays dsa/rotating arrays main/Rotate_and_removeLast.java
@@ -2,21 +2,23 @@ import java.util.Arrays;
 class Rotate_and_removeLast
 {
     public static void main(String[] args) {
-        
+        int i;
         int arr[]={1,2,3,4,5};
         int n=arr.length-1;
 
-        for(int i=0;i<n;i++)
+        for(i=0;i<n;i++)
         {
             int temp=arr[i];
             arr[i]=arr[n];
             arr[n]=temp;
         }
        
-       
+       if(i==1) continue;
+    
 
        
         System.out.println(Arrays.toString(arr) );
        }
+       }
         
-    }
+    
