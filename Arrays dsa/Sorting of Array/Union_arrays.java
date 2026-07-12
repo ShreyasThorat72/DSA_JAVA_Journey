5e6190f17d0d88daba390585cc19de470800247e
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-07-12 13:07:51 +0530

    added

--- Arrays dsa/Sorting of Array/Union_arrays.java
+++ Arrays dsa/Sorting of Array/Union_arrays.java
@@ -4,24 +4,30 @@ class Union_arrays
 public static void main(String[] args) {
     int arr[]={1,5,3,4,6};
     
-    
-    for(int i=0;i<arr.length-1;i++)
+    int arr2[]={3,4,9};
+
+    int temp=arr[0];
+    for(int i=0;i<arr.length;i++)
     {
-        for(int j=0;j<arr.length-1;j++)
-        {
-        if(arr[j]>arr[j+1])
-        {
-        int temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
-
-        }
-     
-
-        }
+     System.out.println(arr[i]);
     }
-                   System.out.println(Arrays.toString(arr));
+    for(int j=0;j<arr2.length;j++)
+        {
+         System.out.println(arr2[j]+" end   ");
 
+        }
+    
+    int[] newArray = new int[arr.length+arr2.length]; 
+ 
+ for(int i=0;i<arr.length;i++)
+ {
+  newArray[i]=arr[i];
+ }
+ for(int j=0;j<arr2.length;j++)
+ {
+    newArray[arr.length+j]=arr2[j];
+ }
+    System.out.println(Arrays.toString(newArray));              
     
 }
 
