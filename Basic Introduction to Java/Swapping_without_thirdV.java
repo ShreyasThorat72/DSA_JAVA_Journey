e09f739658937b14ecacfffaf24032df48674948
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-07-17 19:54:12 +0530

    swapped two number without using thirs variable

---
+++ Basic Introduction to Java/Swapping_without_thirdV.java
@@ -0,0 +1,14 @@
+class Swapping_without_thirdV
+{
+    public static void main(String[] args) {
+       int i=9;
+       int j=11;
+
+      int arr[]={i,j};
+       for(int k=0;k<arr.length-1;k++)
+       {
+        System.out.println(arr[arr.length-1]+" "+arr[0]);
+       } 
+    }
+
+}
