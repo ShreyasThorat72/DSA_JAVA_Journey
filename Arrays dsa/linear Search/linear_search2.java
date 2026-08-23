8b3d0859c07dc37f4f28cfe8efad16ed5772366d
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-23 10:45:35 +0530

    reduced time compkextiy

--- Arrays dsa/linear Search/linear_search2.java
+++ Arrays dsa/linear Search/linear_search2.java
@@ -2,17 +2,35 @@ class linear_search2
 {
     public static void main(String[] args) {
         int arr[]={1,3,4,5,6,2};
+        int target=6;
+
+        int ans=linearSearch(arr, target);
+
+        System.out.println("element found at index :- "+ans);
+
+
+
+
+    }
+    static int linearSearch(int arr[],int target)
+    {
+     
+     if(arr.length==0)
+     {
+        return -1;
+     }
+    
         int temp=0;
         
         for(int i=0;i<arr.length;i++)
         {
-        if(temp==5)
+        int element =arr[i];
+        if(element==target)
         {
-            temp=arr[i];
-            
+            return i;
         }
      
         }
-           System.out.println("the key value is found at index : "+" "+"is the number: "+temp);
+          return -1;
     }
 }
