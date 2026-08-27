4ed2d73fd0d324188631405916e459c1ed1ea7d8
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-27 20:13:58 +0530

    Change variable 'b' from 7 to 8 in substract.java

--- Basic Introduction to Java/substract.java
+++ Basic Introduction to Java/substract.java
@@ -2,7 +2,7 @@ public class SimpleAdd
 {
     public static void main(String[] args) {
         int a=5;
-        int b=7;
+        int b=8;
         int c=a-b;
         System.out.println(".(addition of the numbers is :)"+c);
     }
