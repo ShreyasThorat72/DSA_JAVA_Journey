d23770e17a845dcf4c98778b4862ae02874330c0
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-27 20:05:16 +0530

    Update ContinueStmt.java
    changed some basic things

--- jump Statements/ContinueStmt.java
+++ jump Statements/ContinueStmt.java
@@ -2,7 +2,7 @@ class ContinueStmt
 {
   public static void main(String[] args) {
       {
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 7; i++) {
             if(i==3)
             {
                 System.out.println("  ");
