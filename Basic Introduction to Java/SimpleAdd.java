08cd8faa1a0285d3606c191730de9d2cca14847a
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-27 20:02:41 +0530

    Update SimpleAdd.java
    changed values

--- Basic Introduction to Java/SimpleAdd.java
+++ Basic Introduction to Java/SimpleAdd.java
@@ -2,7 +2,7 @@ public class SimpleAdd
 {
     public static void main(String[] args) {
         int a=5;
-        int b=6;
+        int b=7;
         int c=a+b;
         System.out.println(".(addition of the numbers is :)"+c);
     }
