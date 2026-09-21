f8f845a0b78a2a760e35566f6508afec0d7ec875
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-09-21 20:33:46 +0530

    added normal for loop

---
+++ Arrays dsa/Leetcode_fundamentals/Traversing an Array/normal_for_loop.java
@@ -0,0 +1,12 @@
+class normal_for_loop
+{
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40};
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(arr[i]);
+            
+        }
+    }
+}
