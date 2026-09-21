0143de81893b11169564497180d87c5f1aa98986
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-09-21 20:19:52 +0530

    updated an element

---
+++ Arrays dsa/Leetcode_fundamentals/updating elements/Element_update.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Element_update
+{
+    public static void main(String[] args) {
+        
+        int arr[]={2,3,4,5,6,7,8,9};
+
+         arr[0]=4;
+
+         System.out.println(Arrays.toString(arr));
+
+    }
+}
