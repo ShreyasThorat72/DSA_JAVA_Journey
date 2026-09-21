93c60a3d76ab438876e3dd8719cedb3bb18ddf04
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-09-21 20:38:08 +0530

    learned new concpt of traversing array using Enhanced for loop

---
+++ Arrays dsa/Leetcode_fundamentals/Traversing an Array/enhanced_for_loop.java
@@ -0,0 +1,12 @@
+class enhanced_for_loop
+{
+    public static void main(String[] args) {
+        
+        int arr[]={2,3,4,5,6};
+
+        for(int x:arr)
+        {
+            System.out.println(x);
+        }
+    }
+}
