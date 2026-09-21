4474944537d03b886134526efd4ed36ff2f2e368
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-09-21 20:13:30 +0530

    Accessig elements using index

---
+++ Arrays dsa/Leetcode_fundamentals/Accesing elements/basic_Accesing.java
@@ -0,0 +1,9 @@
+class basic_Accesing
+
+{
+    public static void main(String args[])
+    {
+        int arr[]={2,4,5,67,8};
+        System.out.println("accesing the third element of array:- "+arr[2]);
+    }
+}
