7d37efd16a4cceb6ebd170579b6983f00234f33e
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-09-21 20:29:41 +0530

    length of array is seen

---
+++ Arrays dsa/Leetcode_fundamentals/Array length/array_length.java
@@ -0,0 +1,9 @@
+class array_length
+{
+    public static void main (String args[])
+    {
+        int arr[]={2,4,5,67,8};
+
+        System.out.println("finding length of an array :-"+arr.length);
+    }
+}
